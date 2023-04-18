e8cc51d7c234a18ddf318ee162efe8425081c316
julia.azevedolins@gmail.com <julia.azevedolins@gmail.com>
2023-04-18 20:59:23 -0300

    Execícios de aula do dia 18.04

--- js/script.js
+++ js/script.js
@@ -19,4 +19,47 @@ function soma(){
 
 }
 
-soma()
+//Recuperando elementos do HTML com querySelector
+// const liElements = document.querySelector("li");
+// console.log(liElements);
+
+//Recuperando elementos do HTML com querySelectorAll
+//const liElements = [...document.querySelectorAll("li")];
+
+//Convertendo NodeList em array COM SPREAD
+//const liElements = [...document.querySelectorAll("li")];
+// const arrayElements = [...liElements];
+// console.log(arrayElements);
+
+
+// const arrayElements = [...document.querySelectorAll("li")];
+
+// arrayElements.map((el,key)=>{
+
+//     if (el.textContent =="item-24") {
+//         el.textContent = `${key+1}[ <-- É esse aqui oh ]`; //Texto
+//     }
+
+// });
+let tmp = 0;
+
+
+function mudaCor() {
+    let r = Math.ceil(Math.random()*255);
+    let g = Math.ceil(Math.random()*255);
+    let b = Math.ceil(Math.random()*255);
+    
+    const elemento = [...document.getElementsByClassName("conteudo")];
+    elemento.forEach( (el)=>{
+        el.style.backgroundColor = `rgb(${r},${g},${b})`;
+    } )
+
+   tmp = setTimeout(mudaCor, 5000);
+
+}
+
+function pararTimeOut() {
+    clearTimeout(tmp);
+}
+
+mudaCor()
