f2f4cfba8ced7803ab022606d99435553a99cede
JuliaAzevedoLins <julia.azevedolins@gmail.com>
2023-04-25 20:54:53 -0300

    Exercícios feitos em aula - 25/04

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-// Exemplo de Funções
-const divElement = document.getElementById("div-box")
+// // Exemplo de Funções
+// const divElement = document.getElementById("div-box")
 
-// divElement.addEventListener("click",()=>{
-//     console.log(divElement);
-// });
+// // divElement.addEventListener("click",()=>{
+// //     console.log(divElement);
+// // });
 
 
-divElement.addEventListener("click", mostraElemento);
+// divElement.addEventListener("click", mostraElemento);
 
-function mostraElemento() {
-    console.log(divElement)
-}
+// function mostraElemento() {
+//     console.log(divElement)
+// }
 
-function soma(){
-    let n1 = 10
-    let n2 = 5
-    console.log(n1 + n2)
+// function soma(){
+//     let n1 = 10
+//     let n2 = 5
+//     console.log(n1 + n2)
 
-}
+// }
 
 //Recuperando elementos do HTML com querySelector
 // const liElements = document.querySelector("li");
@@ -41,8 +41,27 @@ function soma(){
 //     }
 
 // });
-let tmp = 0;
+// let tmp = 0;
 
+
+// function mudaCor() {  //para mudar cores
+//      let r = Math.ceil(Math.random()*255);
+//      let g = Math.ceil(Math.random()*255);
+//      let b = Math.ceil(Math.random()*255);
+    
+//      const elemento = [...document.getElementsByClassName("conteudo")];
+//      elemento.forEach( (el)=>{
+//          el.style.backgroundColor = `rgb(${r},${g},${b})`;
+//      } )
+
+//     tmp = setTimeout(mudaCor, 5000);
+
+//  }
+
+// ----------------------- AULA DO DIA 25.04 ----------------------
+//----------------------- ADICIONANDO IMAGENS ---------------------
+
+let tmp = 0;
 
 function mudaCor() {
     let r = Math.ceil(Math.random()*255);
@@ -54,12 +73,42 @@ function mudaCor() {
         el.style.backgroundColor = `rgb(${r},${g},${b})`;
     } )
 
-   tmp = setTimeout(mudaCor, 5000);
+   tmp = setTimeout(mudaCor, 1000);
 
 }
 
-function pararTimeOut() {
-    clearTimeout(tmp);
+function alteraBanner1() {
+    
+    // let nr = Math.ceil(Math.random() *3);
+    const img1 = document.querySelector(".l-d > img");
+    const img2 = document.querySelector(".l-e > img");
+    img1.src = "../RM98690/img/banner-lateral-1.png";
+    img2.src = "../RM98690/img/banner-lateral-2.png";
+
+    setTimeout(alteraBanner2, 1000);
+
+}
+ 
+function alteraBanner2() {
+    
+    // let nr = Math.ceil(Math.random() *3);
+    const img1 = document.querySelector(".l-d > img");
+    const img2 = document.querySelector(".l-e > img");
+    img1.src = "../RM98690/img/banner-lateral-1.png";
+    img2.src = "../RM98690/img/banner-lateral-2.png";
+
+    setTimeout(alteraBanner3, 1000);
+
 }
 
-mudaCor()
+function alteraBanner3() {
+    
+    // let nr = Math.ceil(Math.random() *3);
+    const img1 = document.querySelector(".l-d > img");
+    const img2 = document.querySelector(".l-e > img");
+    img1.src = "../RM98690/img/banner-lateral-1.png";
+    img2.src = "../RM98690/img/banner-lateral-2.png";
+
+    setTimeout(alteraBanner1, 1000);
+
+}
